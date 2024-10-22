de995d403815ca0f96dcf0f178d3e724bdfbf7ed
Camille L <b.lemortcamille@gmail.com>
2024-10-22 16:25:02 +0200

    Bulbasaur

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,8 +1,10 @@
 function PokemonCard () {
+    return (
     <figure>
-        <img>https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png</img>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img>
         <figcaption>Bulbasaur</figcaption>
         </figure>
-}
+
+)}
 
 export default PokemonCard;
